9076b733325833b40a07303c0c2c9a5245d9a3a4
David Betteridge <davidabetteridge@gmail.com>
2021-12-01 08:37:38 +0000

    Day 1 - Part 2

--- Day01/day1.py
+++ Day01/day1.py
@@ -1,12 +1,23 @@
 from typing import List
 
-def read_file(filename) -> List[str]:
+def read_file(filename) -> List[int]:
   with open(filename) as f:
-    return f.readlines()
+    lines = f.readlines()
+    return list(map(int, lines))
 
+def part1():
+  depths = read_file("Day01/data.txt")
+  tuples = zip(depths, depths[1:])
+  bigger = len([z for z in tuples if z[1]>z[0]])
+  print(bigger)
 
-depths = read_file("Day01/data.txt")
-depths = list(map(int, depths))
-tuples = zip(depths, depths[1:])
-bigger = len([z for z in tuples if z[1]>z[0]])
-print(bigger)
+def part2():
+  depths = read_file("Day01/data.txt")
+  tuples = zip(depths, depths[1:], depths[2:])
+  windows = [sum(z) for z in tuples]
+  window_tuples = zip(windows, windows[1:])
+  bigger = len([z for z in window_tuples if z[1]>z[0]])
+  print(bigger)
+
+part1()
+part2()
