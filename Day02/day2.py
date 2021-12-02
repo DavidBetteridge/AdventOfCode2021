e79554cda843f9db08d7f400d31d14e548b0ac90
David Betteridge <davidabetteridge@gmail.com>
2021-12-02 11:01:26 +0000

    Refactor

--- Day02/day2.py
+++ Day02/day2.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 def part1():
   data = pd.read_csv("Day02/data.txt", header = None, names = ["command", "amount"], sep=" ")
@@ -9,31 +10,24 @@ def part1():
 
 def part2():
   data = pd.read_csv("Day02/data.txt", header = None, names = ["command", "amount"], sep=" ")
-  data["depth_down"] = data.loc[data.command == "down"].amount 
-  data["depth_up"] = data.loc[data.command == "up"].amount
-  data["forward_distance"] = data.loc[data.command == "forward"].amount
-  data = data.fillna(0)
-  data["aim_change"] = data.depth_down - data.depth_up
-  data["aim"] = data.aim_change.cumsum()
-  data["x_pos"] = data.forward_distance.cumsum()
-  data["depth_change"] = data[data.command == "forward"].apply(lambda row: row.aim * row.amount, axis=1)
-  data = data.fillna(0)
-  data["depth"] = data.depth_change.cumsum()
+  data["aim"] = np.where(data.command == "up", -data.amount, np.where(data.command == "down", data.amount, 0)).cumsum()
+  data["x_pos"] = data.loc[data.command == "forward"].amount.cumsum()
+  data["depth"] = data[data.command == "forward"].apply(lambda row: row.aim * row.amount, axis=1).cumsum()
   print(data.iloc[-1].x_pos * data.iloc[-1].depth)
 
+part2()
 
-
-aim = 0
-x_pos = 0
-depth = 0
-for row in data.itertuples(index=True, name='Pandas'):
-  if row.command == "down":
-    aim += row.amount
-  elif row.command == "up":
-    aim -= row.amount
-  elif row.command == "forward":
-    x_pos += row.amount
-    depth += aim * row.amount
-print(data)
-print(x_pos * depth)   #1857958050
+# aim = 0
+# x_pos = 0
+# depth = 0
+# for row in data.itertuples(index=True, name='Pandas'):
+#   if row.command == "down":
+#     aim += row.amount
+#   elif row.command == "up":
+#     aim -= row.amount
+#   elif row.command == "forward":
+#     x_pos += row.amount
+#     depth += aim * row.amount
+# print(data)
+# print(x_pos * depth)   #1857958050
 
