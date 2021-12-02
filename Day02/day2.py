ffd837c7211095c540c60b7e68efef93c4b55fdb
David Betteridge <davidabetteridge@gmail.com>
2021-12-02 08:47:35 +0000

    Day 2 - Part 2

--- Day02/day2.py
+++ Day02/day2.py
@@ -1,8 +1,23 @@
 import pandas as pd
 
+def part1():
+  data = pd.read_csv("Day02/data.txt", header = None, names = ["command", "amount"], sep=" ")
+  forward = data.loc[data.command == "forward"].amount.sum()
+  depth = data.loc[data.command == "down"].amount.sum() - data.loc[data.command == "up"].amount.sum() 
+  print(forward * depth)   # 1693300
+
+
 data = pd.read_csv("Day02/data.txt", header = None, names = ["command", "amount"], sep=" ")
-
-forward = data.loc[data.command == "forward"].amount.sum()
-depth = data.loc[data.command == "down"].amount.sum() - data.loc[data.command == "up"].amount.sum() 
-
-print(forward * depth)
+aim = 0
+x_pos = 0
+depth = 0
+for row in data.itertuples(index=True, name='Pandas'):
+  if row.command == "down":
+    aim += row.amount
+  elif row.command == "up":
+    aim -= row.amount
+  elif row.command == "forward":
+    x_pos += row.amount
+    depth += aim * row.amount
+  print(aim, x_pos, depth)
+print(x_pos * depth)
