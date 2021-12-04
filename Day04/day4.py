d89b0ceb8553b90aac84b54d0a22a346cb86d0c1
David Betteridge <davidabetteridge@gmail.com>
2021-12-04 12:25:20 +0000

    refactor

--- Day04/day4.py
+++ Day04/day4.py
@@ -2,22 +2,20 @@
 from typing import List, Tuple
 
 def is_winning_board(board):
-  for row in board:
-    if all([c == "" for c in row]):
-      return True
+  # Completed Row
+  if any(sum(row) == 0 for row in board):
+    return True
 
-  for c in range(5):
-    column = [board[r][c] for r in range(5)]
-    if all([r == "" for r in column]):
-      return True
+  # Completed Column
+  if any(sum([board[r][c] for r in range(5)]) == 0 for c in range(5)):
+    return True
 
   return False
 
 def score_board(board):
   return sum([column 
               for row in board
-              for column in row
-              if column != ""])
+              for column in row])
 
 def read_file() -> Tuple[List[int], List]:
   with open("Day04/data.txt") as f:
@@ -30,13 +28,7 @@ def read_file() -> Tuple[List[int], List]:
     while line_number < len(lines):
       current_board = []
       for _ in range(5):
-        row = [
-          int(lines[line_number][:2]),
-          int(lines[line_number][2:5]),
-          int(lines[line_number][5:8]),
-          int(lines[line_number][8:11]),
-          int(lines[line_number][11:14]),
-        ]
+        row = [int(lines[line_number][i:i+2]) for i in range(0, len(lines[line_number]), 3)]
         current_board.append(row)
         line_number +=1
       boards.append(current_board)
@@ -50,15 +42,16 @@ def play(calls, boards, number_of_remaining_boards_needed):
   for call in calls:
     for board_number, board in enumerate(boards):
       if board_number in remaining_boards:
-        for row in board:
-          for column in range(len(row)):
-            if row[column] == call:
-              row[column] = ""
-              if is_winning_board(board):
-                remaining_boards.remove(board_number)
-                if len(remaining_boards) == number_of_remaining_boards_needed:
-                  print(score_board(board) * call) 
-                  return
+
+        updated_board = [[cell if cell != call else 0 for cell in row]
+                          for row in board]
+        boards[board_number] = updated_board
+
+        if is_winning_board(updated_board):
+          remaining_boards.remove(board_number)
+          if len(remaining_boards) == number_of_remaining_boards_needed:
+            print(score_board(board) * call) 
+            return
 
 calls, boards = read_file()
 play(calls, boards, number_of_remaining_boards_needed = len(boards) - 1)  #6592
