d140341e4e69c698cb927cf8a681e4df27309371
David Betteridge <davidabetteridge@gmail.com>
2021-12-04 11:20:57 +0000

    Refactor

--- Day04/day4.py
+++ Day04/day4.py
@@ -1,5 +1,5 @@
 
-from typing import List
+from typing import List, Tuple
 
 def is_winning_board(board):
   for row in board:
@@ -19,40 +19,47 @@ def score_board(board):
               for column in row
               if column != ""])
 
-def read_file() -> List[str]:
+def read_file() -> Tuple[List[int], List]:
   with open("Day04/data.txt") as f:
-    return f.readlines()
+    lines = f.readlines()
+    calls = list(map(int, lines[0].split(",")))
 
-lines = read_file()
-calls = list(map(int, lines[0].split(",")))
+    line_number = 2
+    boards = []
 
-line_number = 2
-boards = []
+    while line_number < len(lines):
+      current_board = []
+      for _ in range(5):
+        row = [
+          int(lines[line_number][:2]),
+          int(lines[line_number][2:5]),
+          int(lines[line_number][5:8]),
+          int(lines[line_number][8:11]),
+          int(lines[line_number][11:14]),
+        ]
+        current_board.append(row)
+        line_number +=1
+      boards.append(current_board)
 
-while line_number < len(lines):
-  current_board = []
-  for _ in range(5):
-    row = [
-      int(lines[line_number][:2]),
-      int(lines[line_number][2:5]),
-      int(lines[line_number][5:8]),
-      int(lines[line_number][8:11]),
-      int(lines[line_number][11:14]),
-    ]
-    current_board.append(row)
-    line_number +=1
-  boards.append(current_board)
-
-  line_number += 1
+      line_number += 1
+  return calls, boards
 
 
-for call in calls:
-  for board in boards:
-    for row in board:
-      for i in range(len(row)):
-        if row[i] == call:
-          row[i] = ""
-          if is_winning_board(board):
-            print(board)
-            print(score_board(board) * call)  #11312
-            print(score_board(board))
+def play(calls, boards, number_of_renaming_boards_needed):
+  remainining_boards = set([b for b in range(len(boards))])
+  for call in calls:
+    for board_number, board in enumerate(boards):
+      if board_number in remainining_boards:
+        for row in board:
+          for i in range(len(row)):
+            if row[i] == call:
+              row[i] = ""
+              if is_winning_board(board):
+                remainining_boards.remove(board_number)
+                if len(remainining_boards) == number_of_renaming_boards_needed:
+                  print(score_board(board) * call) 
+                  return
+
+calls, boards = read_file()
+play(calls, boards, number_of_renaming_boards_needed = len(boards) - 1)  #6592
+play(calls, boards, number_of_renaming_boards_needed = 0)    #31755
