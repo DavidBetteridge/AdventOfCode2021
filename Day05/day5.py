5221baa8ef725eea7c9da810d3740c89efa2d48a
David Betteridge <davidabetteridge@gmail.com>
2021-12-05 17:33:59 +0000

    Day 5 - Part 2

--- Day05/day5.py
+++ Day05/day5.py
@@ -9,7 +9,7 @@ def parse_line(line: str) -> Tuple:
   return int(x1), int(y1), int(x2), int(y2.rstrip())
 
 def read_file() -> pd.DataFrame:
-  with open("Day05/data.txt") as f:
+  with open("Day05/sample.txt") as f:
     data = [parse_line(line) for line in f.readlines()]
     return pd.DataFrame(data, columns =['x1', 'y1', 'x2', 'y2'])
 
@@ -25,11 +25,24 @@ for index, row in lines.iterrows():
     for y in range(ystart, yend+1):
       overlaps[y][row["x1"]] += 1
 
-  if row["y1"] == row["y2"]:
+  elif row["y1"] == row["y2"]:
     xstart = min(row["x1"], row["x2"])
     xend = max(row["x1"], row["x2"])    
     for x in range(xstart, xend+1):
       overlaps[row["y1"]][x] += 1
+
+  else:
+    ystep = 1 if row["y2"] >= row["y1"] else -1
+    xstep = 1 if row["x2"] >= row["x1"] else -1
+
+    offset = 0
+    x = row["x1"]
+    y = row["y1"]
+    while x != (row["x2"] + xstep):
+      overlaps[y][x] += 1
+      x += xstep
+      y += ystep
+
 
 danger = 0
 for row in range(vertical_size):
@@ -37,4 +50,4 @@ for row in range(vertical_size):
     if overlaps[row][column] >= 2:
       danger +=1
 
-print(danger)  #980100
+print(danger)  #18605
