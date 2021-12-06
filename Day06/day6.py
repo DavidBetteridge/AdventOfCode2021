d1dae3e52c9e14523cfdc790dd7cab1aa8619faa
David Betteridge <davidabetteridge@gmail.com>
2021-12-06 18:15:41 +0000

    Day 6 - Part 2

--- Day06/day6.py
+++ Day06/day6.py
@@ -1,16 +1,27 @@
-from os import read
-from typing import List
-
+from typing import Counter, List
+from functools import lru_cache
 
 def read_file() -> List[int]:
   with open("Day06/data.txt") as f:
     return list(map(int, f.read().split(",")))
 
-fish = read_file()
+@lru_cache()
+def solve_for_single_fish(start_value, days_remaining):
+  if days_remaining == 0: return 1
 
-for day in range(1, 81):
-  fish = [f-1 for f in fish]
-  fish = fish + [8] * len([1 for f in fish if f == -1])
-  fish = [6 if f == -1 else f for f in fish]
-  # print(f"After {day} day: {fish}")
-print(len(fish))  #365131
+  start_value = start_value - 1
+  if start_value == -1:
+    start_value = 6
+    return solve_for_single_fish(start_value, days_remaining - 1) + solve_for_single_fish(8, days_remaining - 1)
+  else:
+    return solve_for_single_fish(start_value, days_remaining - 1)
+
+def solve(fish, days_remaining):
+  quantities = Counter(fish)
+  total = 0
+  for start_number in range (1,6):
+    total += (quantities[start_number] * solve_for_single_fish(start_number, days_remaining))
+  return total
+
+fish = read_file()
+print(solve(fish, 256))   #1650309278600
