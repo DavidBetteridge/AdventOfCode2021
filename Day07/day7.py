7a03c8ae1e7541e0248c74374b42d5cf13125343
David Betteridge <davidabetteridge@gmail.com>
2021-12-07 09:02:46 +0000

    Combined

--- Day07/day7.py
+++ Day07/day7.py
@@ -1,6 +1,6 @@
 from functools import lru_cache
 from typing import List
-
+import timeit
 
 def read_file() -> List[int]:
   with open("Day07/data.txt") as f:
@@ -20,24 +20,42 @@ def part1():
 
 
 
-@lru_cache
-def cost(n: int) -> int:
-  return (n * ( n + 1 )) / 2
+
 
 def part2():
+  @lru_cache
+  def cost(n: int) -> int:
+    return (n * ( n + 1 )) / 2
+
   positions = read_file()
   min_x = min(positions)
   max_x = max(positions)
+  
+  return min([sum( cost(abs(x - p))
+              for p in positions)
+              for x in range(min_x, max_x+1)])
 
-  best_score = 99999999
-  for x in range(min_x, max_x+1):
-    score = sum( cost(abs(x - p)) for p in positions )
-    best_score = min(best_score, score)
+def part2b():
+  def cost(n: int) -> int:
+    return (n * ( n + 1 )) / 2
 
-  print(best_score)   #96086265
+  positions = read_file()
+  min_x = min(positions)
+  max_x = max(positions)
+  
+  return min([sum( cost(abs(x - p))
+              for p in positions)
+              for x in range(min_x, max_x+1)])
 
 
-part2()
+def solve(cost):
+  positions = read_file()
+  min_x = min(positions)
+  max_x = max(positions)
+  
+  return min([sum( cost(abs(x - p))
+              for p in positions)
+              for x in range(min_x, max_x+1)])
 
-
-
+print(solve(lambda n: n))  #Part 1
+print(solve(lambda n: (n * ( n + 1 )) / 2))  #Part 2
