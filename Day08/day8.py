aec1778e6ff768254793b1c6353281579f866153
David Betteridge <davidabetteridge@gmail.com>
2021-12-08 18:07:02 +0000

    Refactor

--- Day08/day8.py
+++ Day08/day8.py
@@ -13,21 +13,27 @@ def read_file() -> List[Tuple[List[str], List[str]]]:
 def part1():
   line = read_file()
   running_total = 0
-  for inputs, outputs in line:
+  for _, outputs in line:
     lengths = [len(o) for o in outputs] 
     c = Counter(lengths)
     running_total += c[2] + c[3] + c[4] + c[7]
   print(running_total)  #456
   
 
+def where(all_possibilities: List[str],
+          length_is: int, 
+          must_contain: List[str] = [],
+          must_not_contain: List[str] = []):
+  possible = [o for o in all_possibilities 
+              if len(o) == length_is and \
+                 all(must in o for must in must_contain) and \
+                 not any(must in o for must in must_not_contain)]
+  return "".join(sorted(set(possible[0])))
 
-lines = read_file()
-running_total = 0
-for inputs, outputs in lines:
-  all = inputs + outputs
-  unique = set(["".join(sorted(set(a))) for a in all])
-  unique_joined = "".join(unique)
-  counts = Counter(unique_joined)  
+def solve_line(inputs, outputs):
+  all_possibilities = inputs + outputs
+  unique = set(["".join(sorted(set(a))) for a in all_possibilities])
+  counts = Counter("".join(unique))  
 
   # b is the letter which is in exactly 6 segments
   for k in counts:
@@ -41,56 +47,25 @@ for inputs, outputs in lines:
       segment_e = k
       break
 
-  
-  # Number 1 - has length 2
-  digits_for_number_one = "".join(sorted(set(next(o for o in all if len(o) == 2))))
+  mapping = {}
+  mapping[1] = where(all_possibilities, length_is=2)
+  mapping[2] = where(all_possibilities, length_is=5, must_contain=[segment_e], must_not_contain=[segment_b])
+  mapping[3] = where(all_possibilities, length_is=5, must_not_contain=[segment_b, segment_e])
+  mapping[4] = where(all_possibilities, length_is=4)
+  mapping[5] = where(all_possibilities, length_is=5, must_contain=[segment_b])
+  mapping[7] = where(all_possibilities, length_is=3)
+  mapping[8] = where(all_possibilities, length_is=7)
+  mapping[9] = where(all_possibilities, length_is=6, must_not_contain=[segment_e])
+  segment_d = set(mapping[4]).difference(set(mapping[1])).difference([segment_b]).pop()
+  mapping[0] = where(all_possibilities, length_is=6, must_not_contain=[segment_d])
+  mapping[6] = unique.difference([mapping[0], mapping[1], mapping[2], mapping[3],
+                                              mapping[4], mapping[5], mapping[7],
+                                              mapping[8], mapping[9]]).pop()
+  mapping = {v:str(k) for k,v in sorted(mapping.items())}
 
-  # Number 2 - has length 5 and contains e but not b
-  digits_for_number_two = "".join(sorted(set(next(o for o in all if len(o) == 5 and segment_e in o and segment_b not in o)) ))
-
-  # Number 3 - has length 5 and no e or b
-  digits_for_number_three = "".join(sorted(set(next(o for o in all if len(o) == 5 and segment_e not in o and segment_b not in o)) ))
-
-  # Number 4 - has length 4
-  digits_for_number_four = "".join(sorted(set(next(o for o in all if len(o) == 4))))
-
-  # Number 5 - has length 5 and contains b
-  digits_for_number_five = "".join(sorted(set(next(o for o in all if len(o) == 5 and segment_b in o))))
-
-  # Number 7 - has length 3
-  digits_for_number_seven = "".join(sorted(set(next(o for o in all if len(o) == 3))))
-
-  # Number 8 - has length 7
-  digits_for_number_eight = "".join(sorted(set(next(o for o in all if len(o) == 7))))
-
-  # Number 9 - has length 8 and no segment_e
-  digits_for_number_nine = "".join(sorted(set(next(o for o in all if len(o) == 6 and segment_e not in o))))
-
-  segment_d = set(digits_for_number_four).difference(set(digits_for_number_one)).difference([segment_b]).pop()
-
-  digits_for_number_zero = "".join(sorted(set(next(o for o in all if len(o) == 6 and segment_d not in o))))
-
-  digits_for_number_six = unique.difference([digits_for_number_zero, digits_for_number_one, digits_for_number_two, digits_for_number_three,
-                                             digits_for_number_four, digits_for_number_five, digits_for_number_seven,
-                                             digits_for_number_eight, digits_for_number_nine]).pop()
+  return int("".join(mapping["".join(sorted(o))] for o in outputs))
 
 
-  m = {
-    digits_for_number_zero: "0",
-    digits_for_number_one: "1",
-    digits_for_number_two: "2",
-    digits_for_number_three: "3",
-    digits_for_number_four: "4",
-    digits_for_number_five: "5",
-    digits_for_number_six: "6",
-    digits_for_number_seven: "7",
-    digits_for_number_eight: "8",
-    digits_for_number_nine: "9",
-  }
-
-  result = ""
-  for o in outputs:
-    result += m["".join(sorted(o))]
-  running_total += int(result)
-
-print(running_total)
+lines = read_file()
+running_total = sum(solve_line(inputs, outputs) for inputs, outputs in lines)
+print(running_total)  #1091609
