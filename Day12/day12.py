7f03d4fdaa4e590353af3cf5d46f9a7069c9f1be
David Betteridge <davidabetteridge@gmail.com>
2021-12-12 12:33:42 +0000

    Day 12 Part 2

--- Day12/day12.py
+++ Day12/day12.py
@@ -13,33 +13,40 @@ for line in read_file("Day12/data.txt"):
   G.add_edge(start, end)
 
 
-def visit(G, node, path: str, routes: Set):
+def visit(G, node, path: str, routes: Set, small_cave_visited_twice: bool):
   for edge in G.edges(node):
+    scvt = small_cave_visited_twice
     to_edge = edge[1]
     if to_edge == "end":
       routes.add(path + "/" + to_edge)
     else:
-      available = to_edge.isupper() or f"/{to_edge}/" not in path
-      if f"{node}/{to_edge}" not in path and available:
-        visit(G, to_edge, path + "/" + to_edge, routes)
+      is_start = to_edge == "start"
+      is_small_cave = to_edge.islower()
+      already_visited = f"/{to_edge}/" in path
+
+      if is_start:
+        available = False
+      else:
+        if is_small_cave:
+          if scvt:
+            available = not already_visited
+          else:
+            available = True
+            scvt = already_visited
+        else: 
+          available = True
+
+      if available:
+        visit(G, to_edge, path + "/" + to_edge, routes, scvt)
 
 
 path = "/start"
 routes = set()
-visit(G, "start", path, routes)
-
-for a in routes:
-  print(a)
-
+visit(G, "start", path, routes, small_cave_visited_twice = True)
 print(len(routes))
 
-# start,A,b,A,c,A,end
-# start,A,b,A,end
-# start,A,b,end
-# start,A,c,A,b,A,end
-# start,A,c,A,b,end
-# start,A,c,A,end
-# start,A,end
-# start,b,A,c,A,end
-# start,b,A,end
-# start,b,end
+
+path = "/start"
+routes = set()
+visit(G, "start", path, routes, small_cave_visited_twice = False)
+print(len(routes))
