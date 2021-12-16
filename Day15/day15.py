129a71323cc60fc6737150fa18d0ae7c0aa514a4
David Betteridge <davidabetteridge@gmail.com>
2021-12-16 23:35:17 +0000

    Day 15 - Part 2

--- Day15/day15.py
+++ Day15/day15.py
@@ -8,34 +8,91 @@ def read_file(filename: str) -> pd.DataFrame:
   return pd.read_fwf(filename, widths=[1]*columns, header = None)  
 
 
-def build_graph(cave: pd.DataFrame) -> nx.DiGraph:
+def build_graph(cave: pd.DataFrame, number_of_tiles: int) -> nx.DiGraph:
   G = nx.DiGraph()
   number_of_rows, number_of_columns = cave.shape
 
   neighbours = [(0,-1), (-1, 0), (1,0), (0,1)]
+  weights = {}
 
-  for row in range(number_of_rows):
-    for column in range(number_of_columns):
-      for col_offset, row_offset in neighbours:
-        if (0 <= column + col_offset < number_of_columns) and \
-            (0 <= row + row_offset < number_of_rows):                
-              G.add_edge( (column,row),
-                          (column + col_offset,row + row_offset),
-                          weight = cave[column + col_offset][row + row_offset])
-  return G
+  for x_tile in range(number_of_tiles):
+    x_tile_offset = x_tile * number_of_columns
+    for y_tile in range(number_of_tiles):
+      y_tile_offset = y_tile * number_of_rows
+      for row in range(number_of_rows):
+        for column in range(number_of_columns):
+          for col_offset, row_offset in neighbours:
+            if (0 <= column + col_offset < number_of_columns) and \
+                (0 <= row + row_offset < number_of_rows):
+                  from_cell = (column + x_tile_offset, row + y_tile_offset)
+                  to_cell = (column + col_offset + x_tile_offset, row + row_offset + y_tile_offset)            
+                  weight = (cave[column + col_offset][row + row_offset] + x_tile + y_tile)
+                  if weight > 9: weight -= 9
+                  weights[to_cell] = weight
+                  G.add_edge(from_cell,
+                             to_cell,
+                             weight = weight)
 
-def solve(repeats: int) -> int:
-  cave = read_file("day15/sample.txt")
-  G = build_graph(cave)
+  for x_tile in range(number_of_tiles):
+    x_tile_offset = x_tile * number_of_columns
+    for y_tile in range(number_of_tiles):
+      y_tile_offset = y_tile * number_of_rows
+      for column in range(number_of_columns):
+        #UP
+        if y_tile > 0:
+          row = 0
+          from_cell = (column + x_tile_offset, row + y_tile_offset)
+          to_cell = (column + x_tile_offset, row + y_tile_offset -1)            
+          G.add_edge(from_cell,
+                    to_cell,
+                    weight = weights[to_cell])
+
+        #DOWN
+        if y_tile+1 < number_of_tiles:
+          row = number_of_rows-1
+          from_cell = (column + x_tile_offset, row + y_tile_offset)
+          to_cell = (column + x_tile_offset, row + y_tile_offset +1)            
+          G.add_edge(from_cell,
+                    to_cell,
+                    weight = weights[to_cell])
+
+      for row in range(number_of_rows):
+        #LEFT
+        if x_tile > 0:
+          column = 0
+          from_cell = (column + x_tile_offset, row + y_tile_offset)
+          to_cell = (column + x_tile_offset - 1, row + y_tile_offset)            
+          G.add_edge(from_cell,
+                    to_cell,
+                    weight = weights[to_cell])
+
+        #RIGHT
+        if x_tile+1 < number_of_tiles:
+          column = number_of_columns-1
+          from_cell = (column + x_tile_offset, row + y_tile_offset)
+          to_cell = (column + x_tile_offset + 1, row + y_tile_offset)            
+          G.add_edge(from_cell,
+                    to_cell,
+                    weight = weights[to_cell])                                               
+
+
+  return G, weights
+
+def solve(number_of_tiles: int) -> int:
+  cave = read_file("day15/data.txt")
+  G, weights = build_graph(cave, number_of_tiles)
   print(G)
 
+
   number_of_rows, number_of_columns = cave.shape
+  x_tile_offset = (number_of_tiles-1) * number_of_columns
+  y_tile_offset = (number_of_tiles-1) * number_of_rows
+
   route = (nx.dijkstra_path(G,
                         source=(0,0),
-                        target=(number_of_columns-1, number_of_rows-1),
+                        target=(x_tile_offset+number_of_columns-1, y_tile_offset+number_of_rows-1),
                         weight='weight'))
-
-  return sum([cave[col][row] for col,row in route if col !=0 or row != 0])                       
+  return sum([weights[(col,row)] for col,row in route if col !=0 or row != 0])                       
 
 
 part_one = solve(1)
@@ -43,4 +100,4 @@ assert part_one in [40, 717]
 
 part_two = solve(5)
 print(part_two)
-assert part_two in [315, 717]
+assert part_two in [315, 2993]   #2199 too low
