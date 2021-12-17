356aff80aaf618b5d08247b6486c3a1fec163b5e
David Betteridge <davidabetteridge@gmail.com>
2021-12-17 23:45:22 +0000

    Refactored

--- Day16/day16.py
+++ Day16/day16.py
@@ -1,20 +1,53 @@
+import math
+
+class BinaryPipe:
+
+  __lookup = {
+    "0" : "0000",
+    "1" : "0001",
+    "2" : "0010",
+    "3" : "0011",
+    "4" : "0100",
+    "5" : "0101",
+    "6" : "0110",
+    "7" : "0111",
+    "8" : "1000",
+    "9" : "1001",
+    "A" : "1010",
+    "B" : "1011",
+    "C" : "1100",
+    "D" : "1101",
+    "E" : "1110",
+    "F" : "1111",
+  }
+
+  def __init__(self, hex_string: str):
+    self.__binary = "".join( [self.__lookup[h] for h in hex_string] )
+    self.__offset = 0
+
+  def read(self, number_of_bits: int) -> int:
+    return int(self.read_as_binary(number_of_bits),2)
+  
+  def read_as_binary(self, number_of_bits: int) -> str:
+    value = self.__binary[self.__offset:self.__offset+number_of_bits]
+    self.__offset += number_of_bits
+    return value
+
+  def current_offset(self) -> int:
+    return self.__offset
+
+
 def read_file(filename: str) -> str:
   with open(filename) as f:
     return f.readline().strip()
 
 
-
-def package_length(binary, start_from=0) -> int:
-  if binary[start_from:].replace("0", "") == "":
-    return 0, len(binary) 
-
+def package_length(binary : BinaryPipe, part1: bool) -> int:
   # Decode packet
-  version = int(binary[start_from:start_from+3],2)
-  start_from+=3
+  version = binary.read(3)
   print(f"version={version}")
 
-  packet_type = int(binary[start_from:start_from+3],2)
-  start_from+=3
+  packet_type = binary.read(3)
   is_literal = packet_type == 4
   print(f"packet_type={packet_type}")
 
@@ -22,39 +55,72 @@ def package_length(binary, start_from=0) -> int:
     done = False
     literal_value = ""
     while not done:
-      group = binary[start_from:start_from+5]
-      done = binary[start_from] == "0"
-      start_from += 5
-      literal_value += group[1:]
+      done = binary.read(1) == 0
+      group = binary.read_as_binary(4)
+      literal_value += group
     print("literal_value", int(literal_value,2))
-    return version, start_from
+    if part1:
+      return version
+    else:
+      return int(literal_value,2)
   else:
     # Operator
-    length_type = int(binary[start_from],2)
-    start_from+=1
+    length_type = binary.read(1)
+    subpacket_values = []
     if length_type == 0:
-      packet_length = int(binary[start_from:start_from+15],2)
-      start_from+=15
+      packet_length = binary.read(15)
       print("packet_length="+str(packet_length))
-      while start_from < len(binary):
-        subpackage_version, start_from = package_length(binary, start_from)   
-        version+=subpackage_version
-      return version, start_from
+      end_of_subpackets = binary.current_offset() + packet_length
+      while binary.current_offset() < end_of_subpackets:
+        subpackage_value = package_length(binary, part1)   
+        if subpackage_value is not None:
+          subpacket_values.append(subpackage_value)
     else:
-      number_of_subpackets = int(binary[start_from:start_from+11],2)
-      start_from+=11
+      number_of_subpackets = binary.read(11)
       print("number_of_subpackets="+str(number_of_subpackets))
 
-      for sp in range(number_of_subpackets):
-        subpackage_version, start_from = package_length(binary, start_from)   
-        version+=subpackage_version
-      return version, start_from
+      for _ in range(number_of_subpackets):
+        subpackage_value = package_length(binary, part1)   
+        if subpackage_value is not None:
+          subpacket_values.append(subpackage_value)
+    
+    if part1:
+      return version + sum(subpacket_values)
+
+    if packet_type == 0:
+      result = sum(subpacket_values)
+
+    if packet_type == 1:
+      result = math.prod(subpacket_values)
+
+    if packet_type == 2:
+      result = min(subpacket_values)
+
+    if packet_type == 3:
+      result = max(subpacket_values)
+
+    if packet_type == 5:
+      result = 1 if subpacket_values[0] > subpacket_values[1] else 0
+
+    if packet_type == 6:
+      result = 1 if subpacket_values[0] < subpacket_values[1] else 0
+
+    if packet_type == 7:
+      result = 1 if subpacket_values[0] == subpacket_values[1] else 0
+
+    return result
 
 
 hex = read_file("Day16/data.txt")
-binary = "".join( [bin(int(h, 16))[2:].zfill(4) for h in hex] )
-print(hex)
-print(binary)
-print(package_length(binary))  #893
+
+pipe = BinaryPipe(hex)
+part1 = package_length(pipe, True)
+print(part1)
+assert part1 == 893
+
+pipe = BinaryPipe(hex)
+part2 = package_length(pipe, False)
+print(part2)
+assert part2 == 4358595186090
 
   
