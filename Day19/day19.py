7bf2b02f40a610fd0876dade27b96949cfc094d8
David Betteridge <davidabetteridge@gmail.com>
2021-12-19 17:43:56 +0000

    Load file

--- Day19/day19.py
+++ Day19/day19.py
@@ -1,3 +1,4 @@
+from typing import List
 import numpy as np
 
 rotations = []
@@ -25,7 +26,7 @@ rotations.append(np.array([
   [0, 1, 0],
   [1, 0, 0]
 ], np.int32))
-##############################
+
 rotations.append(np.array([
   [0, -1, 0],
   [1, 0, 0],
@@ -49,8 +50,6 @@ rotations.append(np.array([
   [1, 0, 0],
   [0, -1, 0]
 ], np.int32))
-#############################
-
 
 rotations.append(np.array([
   [0, 1, 0],
@@ -75,8 +74,6 @@ rotations.append(np.array([
   [-1, 0, 0],
   [0, 1, 0]
 ], np.int32))
-#############################
-
 
 rotations.append(np.array([
   [1, 0, 0],
@@ -102,15 +99,11 @@ rotations.append(np.array([
   [1, 0, 0]
 ], np.int32))
 
-#########################
-
-
 rotations.append(np.array([
   [1, 0, 0],
   [0, -1, 0],
   [0, 0, -1]
 ], np.int32))
-
 
 rotations.append(np.array([
   [0, 0, -1],
@@ -129,8 +122,6 @@ rotations.append(np.array([
   [0, -1, 0],
   [1, 0, 0]
 ], np.int32))
-
-#############################
 
 rotations.append(np.array([
   [1, 0, 0],
@@ -156,11 +147,41 @@ rotations.append(np.array([
   [1, 0, 0]
 ], np.int32))
 
-#############################
+def all_rotations_of_vector(vector: np.array) -> List[np.array]:
+  all = []
+  for rotation in rotations:
+    all.append(np.dot(rotation, vector))
+  return all
+
+class Scanner:
+  def __init__(self, number: int):
+    self.number = number
+    self.orientations = [[] for _ in range(24)]
+  def add_beacon(self, beacon: str):
+    identity = np.array(beacon.split(","), np.int32)
+    for i in range(24):
+      self.orientations[i].append(np.dot(rotations[i], identity))
+
+def parse_file() -> List[Scanner]:
+  with open("day19/sample.txt") as f:
+    lines = f.readlines()
+    scanner_number = -1
+    scanner = None
+    scanners = []
+    for line in lines:
+      #--- scanner 0 ---
+      if line.startswith("---"):
+        if scanner is not None:
+          scanners.append(scanner)
+        scanner = Scanner(scanner_number)
+      elif line.strip() != "":
+        scanner.add_beacon(line.strip())
+    return scanners
+
+
+scanners = parse_file()
+print(scanners.beacons)
 
 beacon = np.array([8,0,7])
-all = set()
-for rotation in rotations:
-  print(np.dot(rotation, beacon))
-  all.add(str(np.dot(rotation, beacon)))
+all = all_rotations_of_vector(beacon)
 print(len(all))
