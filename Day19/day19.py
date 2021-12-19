2c598e448ac223a1cc5db06350fcf72fcb82ab4a
David Betteridge <davidabetteridge@gmail.com>
2021-12-19 18:12:57 +0000

    Use df

--- Day19/day19.py
+++ Day19/day19.py
@@ -1,5 +1,6 @@
 from typing import List
 import numpy as np
+import pandas as pd
 
 rotations = []
 
@@ -147,23 +148,23 @@ rotations.append(np.array([
   [1, 0, 0]
 ], np.int32))
 
-def all_rotations_of_vector(vector: np.array) -> List[np.array]:
-  all = []
-  for rotation in rotations:
-    all.append(np.dot(rotation, vector))
-  return all
-
+#######################################################
 class Scanner:
   def __init__(self, number: int):
     self.number = number
-    self.orientations = [[] for _ in range(24)]
+    self.orientations = [
+      pd.DataFrame(data=[], columns=["x", "y", "z"])
+      for _ in range(24)
+    ]
+
   def add_beacon(self, beacon: str):
     identity = np.array(beacon.split(","), np.int32)
     for i in range(24):
-      self.orientations[i].append(np.dot(rotations[i], identity))
+      new_row = np.dot(rotations[i], identity)
+      self.orientations[i].loc[len(self.orientations[i])] = new_row
 
 def parse_file() -> List[Scanner]:
-  with open("day19/sample.txt") as f:
+  with open("day19/small.txt") as f:
     lines = f.readlines()
     scanner_number = -1
     scanner = None
@@ -176,12 +177,9 @@ def parse_file() -> List[Scanner]:
         scanner = Scanner(scanner_number)
       elif line.strip() != "":
         scanner.add_beacon(line.strip())
+    scanners.append(scanner)
     return scanners
-
+#######################################################
 
 scanners = parse_file()
-print(scanners.beacons)
-
-beacon = np.array([8,0,7])
-all = all_rotations_of_vector(beacon)
-print(len(all))
+print(len(scanners))
