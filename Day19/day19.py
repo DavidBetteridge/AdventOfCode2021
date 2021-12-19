80c7aeb4119bed6b133095b670ec181aca7ccbb9
David Betteridge <davidabetteridge@gmail.com>
2021-12-19 20:13:56 +0000

    finds overlaps slowly

--- Day19/day19.py
+++ Day19/day19.py
@@ -163,8 +163,12 @@ class Scanner:
       new_row = np.dot(rotations[i], identity)
       self.orientations[i].loc[len(self.orientations[i])] = new_row
 
+  # def load_complete(self):
+  #   for i in range(len(self.orientations)):
+  #     self.orientations[i] = self.orientations[i].sort_values(by=['x','y','z'])
+
 def parse_file() -> List[Scanner]:
-  with open("day19/small.txt") as f:
+  with open("day19/sample.txt") as f:
     lines = f.readlines()
     scanner_number = -1
     scanner = None
@@ -178,8 +182,26 @@ def parse_file() -> List[Scanner]:
       elif line.strip() != "":
         scanner.add_beacon(line.strip())
     scanners.append(scanner)
+
+    # for scanner in scanners:
+    #   scanner.load_complete()
     return scanners
 #######################################################
 
 scanners = parse_file()
-print(len(scanners))
+
+fixed = scanners[0]
+compare = scanners[1]
+
+best_number_of_overlaps = 0
+n = compare.orientations[0].shape[0]
+for fixed_index in range(fixed.orientations[0].shape[0]):
+  fixed_b = fixed.orientations[0].loc[fixed_index]
+  for orientation in compare.orientations:
+    for possible_first_overlap_index in range(n):
+      possible_first_overlap = orientation.loc[possible_first_overlap_index]
+      diff = fixed_b - possible_first_overlap
+      adjusted = orientation + diff
+      # number_of_overlaps = pd.merge(fixed.orientations[0], adjusted, how ='inner', on =['x', 'y', 'z']).shape[0]
+      # best_number_of_overlaps = max(best_number_of_overlaps, number_of_overlaps)
+print(best_number_of_overlaps)
