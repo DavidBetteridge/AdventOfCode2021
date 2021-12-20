aca4ad93faad69ab77625a2f7c6ca141e71f4019
David Betteridge <davidabetteridge@gmail.com>
2021-12-20 12:43:08 +0000

    Day 20 - Part 2

--- Day20/day20.py
+++ Day20/day20.py
@@ -1,21 +1,28 @@
-from typing import Counter
+from typing import Counter, List
 
 
 def parse_file():
   with open("data.txt") as f:
     lines = f.readlines()
     image_enhancement_algorithm = lines[0].strip()
-    image = ['......'+l.strip()+'......' for l in lines[2:]]
-    top_and_bottom = "." * len(image[0])
-    return image_enhancement_algorithm, ([top_and_bottom]*6) + image + ([top_and_bottom]*6)
+    image = [l.strip() for l in lines[2:]]
+    return image_enhancement_algorithm, image
+
+def pad_image(image: List[str]) -> List[str]:
+  n = 50
+  image = [('...'*n) + l + ('...'*n)  for l in image]
+  top_and_bottom = "." * len(image[0])
+  return ([top_and_bottom]*(3*n)) + image + ([top_and_bottom]*(3*n))
+
 
 image_enhancement_algorithm, image = parse_file()
+image = pad_image(image)
 
-for _ in range(2):
+for _ in range(50):
   new_image = []
-  for row in range(1, len(image) - 2):
+  for row in range(2, len(image) - 2):
     new_row = ""
-    for column in range(1, len(image[0]) - 2):
+    for column in range(2, len(image[0]) - 2):
       input = image[row-1][column-1] + image[row-1][column] + image[row-1][column+1] +  \
               image[row][column-1] + image[row][column] + image[row][column+1] + \
               image[row+1][column-1] + image[row+1][column] + image[row+1][column+1]
@@ -31,4 +38,4 @@ for i in image:
 
 
 c = Counter("".join([i for i in image]))
-print(c["#"])
+print(c["#"])  #5349
