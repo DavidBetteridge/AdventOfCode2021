04fe29c8edc1c8a50855bb6106f2a0c47bbd031a
David Betteridge <davidabetteridge@gmail.com>
2021-12-30 00:34:05 +0000

    Optimiser

--- Day24/day24.py
+++ Day24/day24.py
@@ -77,11 +77,142 @@ def execute_instruction(instruction: Instruction, state: State) -> State:
 
 
 
+@dataclasses.dataclass(frozen=True)
+class ParseState:
+  w: str = dataclasses.field(default="0")
+  x: str = dataclasses.field(default="0")
+  y: str = dataclasses.field(default="0")
+  z: str = dataclasses.field(default="0")
+  inputNumber: int = dataclasses.field(default=1)
 
+  def getValue(self, variableNameOrValue: str) -> str:
+    if variableNameOrValue in ["w","x", "y", "z"]:
+      return variableNameOrValue
+    else:
+      return variableNameOrValue
+
+  def nextInput(self) -> "ParseState":
+    return ParseState(self.w, self.x, self.y, self.z, self.inputNumber+1)
+
+  def setValue(self, variableName: str,  newValue: int) -> "ParseState":
+    if variableName == "w":
+      return ParseState(newValue, self.x, self.y, self.z, self.inputNumber)
+    elif variableName == "x":
+      return ParseState(self.w, newValue, self.y, self.z, self.inputNumber)
+    elif variableName == "y":
+      return ParseState(self.w, self.x, newValue, self.z, self.inputNumber)
+    elif variableName == "z":
+      return ParseState(self.w, self.x, self.y, newValue, self.inputNumber)
+
+  def getValue(self, variableNameOrValue: str) -> str:
+    if variableNameOrValue == "w":
+      return self.w
+    elif variableNameOrValue == "x":
+      return self.x
+    elif variableNameOrValue == "y":
+      return self.y
+    elif variableNameOrValue == "z":
+      return self.z
+    else:
+      return variableNameOrValue
+
+
+def parse_instruction(instruction: Instruction, state: ParseState) -> ParseState:
+  second = "" if instruction.operand2 is None else instruction.operand2
+  i = f"{instruction.operation} {instruction.operand1} {second}"
+  comment = ""
+
+  if instruction.operation == "inp":
+    state = state.setValue(instruction.operand1, f"INPUT{state.inputNumber}")
+    comment = f"; {instruction.operand1} = INPUT{state.inputNumber}"
+    state = state.nextInput()
+
+  elif instruction.operation == "add":
+    op1 = state.getValue(instruction.operand1)
+    op2 = state.getValue(instruction.operand2)
+    if op1.isdigit() and op2.isdigit():
+      val = int(op1) + int(op2)
+      state = state.setValue(instruction.operand1, str(val))
+      comment = f"; {instruction.operand1} = {val}"
+    elif op1 in ["0", 0]:
+      state = state.setValue(instruction.operand1, op2)
+      comment = f"; {instruction.operand1} = {op2}"      
+    elif op1 == "(INPUT1+14)" and op2 == "13":      
+      state = state.setValue(instruction.operand1, f"(INPUT1+27)")
+      comment = f"; {instruction.operand1} = (INPUT1+27)"      
+    else:      
+      state = state.setValue(instruction.operand1, f"({op1}+{op2})")
+      comment = f"; {instruction.operand1} = {op1}+{op2}"
+
+  elif instruction.operation == "mul":
+    op1 = state.getValue(instruction.operand1)
+    op2 = state.getValue(instruction.operand2)
+    if op1 in ["0", 0] or op2 in ["0", 0]:
+      state = state.setValue(instruction.operand1, "0")
+      comment = f"; {instruction.operand1} = 0"
+    elif op2 in ["1", 1]:
+      state = state.setValue(instruction.operand1, op1)
+      comment = f"; {instruction.operand1} = {op1}"      
+    else:
+      state = state.setValue(instruction.operand1, f"({op1} * {op2})")
+      comment = f"; {instruction.operand1} = {op1} * {op2}"
+
+  elif instruction.operation == "div":
+    op1 = state.getValue(instruction.operand1)
+    op2 = state.getValue(instruction.operand2)
+    if op2 in ["1", 1]:
+      state = state.setValue(instruction.operand1, op1)
+      comment = f"; {instruction.operand1} = {op1}"    
+    else:      
+      state = state.setValue(instruction.operand1, f"({op1} / {op2})")
+      comment = f"; {instruction.operand1} = {op1} / {op2}" 
+
+  elif instruction.operation == "mod":
+    op1 = state.getValue(instruction.operand1)
+    op2 = state.getValue(instruction.operand2)
+    if op1 in ["0", 0]:
+      state = state.setValue(instruction.operand1, "0")
+      comment = f"; {instruction.operand1} = 0"
+    elif op1 == "(INPUT1+14)" and op2 == "26":
+      # Input1 can never be more than 9,  and 9+14 < 26
+      state = state.setValue(instruction.operand1, op1)
+      comment = f"; {instruction.operand1} = {op1}"
+    else:      
+      state = state.setValue(instruction.operand1, f"({op1} mod {op2})")
+      comment = f"; {instruction.operand1} = {op1} mod {op2}"    
+
+  elif instruction.operation == "eql":
+    op1 = state.getValue(instruction.operand1)
+    op2 = state.getValue(instruction.operand2)
+
+    if op2.startswith("INPUT") and op1.isdigit() and (int(op1) > 10 or int(op1) < 1):
+      state = state.setValue(instruction.operand1, "0")
+      comment = f"; {instruction.operand1} = 0"
+    elif op1 == "(INPUT1+27)" and op2=="INPUT2":
+      #INPUT2 can not be more than 9
+      state = state.setValue(instruction.operand1, "0")
+      comment = f"; {instruction.operand1} = 0"      
+    elif op1 == op2:
+      state = state.setValue(instruction.operand1, "1")
+      comment = f"; {instruction.operand1} = 1"      
+    else:
+      state = state.setValue(instruction.operand1, f"(1 if {op1}=={op2} else 0)")
+      comment = f"; {instruction.operand1} = (1 if {op1}=={op2} else 0)"  
+
+  else:
+    print(f"Not supported {instruction.operation}")
+
+  print(f"{i} {comment}")
+  return state
+
+import os
+os.system("cls")
 instructions = parse_file("day24/data.txt")
-state = State()
+state = ParseState()
 for instruction in instructions:
-  state = execute_instruction(instruction, state)
-print(state)
+  state = parse_instruction(instruction, state)
+
 
 # Input 1,    w=input1   x=1,  y=input1+14   z=input1+14
+
+
